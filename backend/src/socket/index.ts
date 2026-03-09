468e8ad732a029778347821f5aed09cf1aa07fbc
d4xaris <katerynaiakhnovska@gmail.com>
2026-03-09 22:28:26 +0200

    fix: fixed structure of the project, can start working now

---
+++ backend/src/socket/index.ts
@@ -0,0 +1,3 @@
+import type { FastifyInstance } from "fastify";
+
+export default function setupSocket(app: FastifyInstance) {}
--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,13 +1,3 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "UNO GAME" },
-  ];
-}
-
 export default function Home() {
-  return <Welcome />;
+  return <h1>Home</h1>;
 }
