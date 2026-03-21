df7ca5dd3053d184c5763ea1856fa2d3b80f6bac
Xonex <prokoptsov.k.v.-imo51@edu.kpi.ua>
2026-03-21 19:40:25 +0200

    feat: добавил базовые типы и класс для колоды UNO

--- backend/src/game/Deck.ts
+++ backend/src/game/Deck.ts
@@ -1,1 +1,15 @@
 export class Deck {}
+type CardColor = 'Red' | 'Green' | 'Blue' | 'Yellow' | 'Black';
+type CardValue = |'0'| '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | 'Skip' | 'Reverse' | 'Draw+2' | 'black-change' | 'black+4-change';
+interface Card{
+    color: CardColor;
+    value: CardValue;
+}
+class Decka {
+    private cards: Card[] = [];
+    constructor() {
+        const colors: CardColor[] = ['Red', 'Green', 'Blue', 'Yellow'];
+        const values: CardValue[] = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', 'Draw+2'];
+
+    }
+}
