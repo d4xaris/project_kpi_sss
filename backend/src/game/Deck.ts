a099360794ae662ba3abfbe0068c6719a1c84284
Xonex <prokoptsov.k.v.-imo51@edu.kpi.ua>
2026-03-22 20:37:41 +0200

    реализовал генерацию карт 108 шт

--- backend/src/game/Deck.ts
+++ backend/src/game/Deck.ts
@@ -7,9 +7,43 @@ interface Card{
 }
 class Decka {
     private cards: Card[] = [];
+
     constructor() {
         const colors: CardColor[] = ['Red', 'Green', 'Blue', 'Yellow'];
-        const values: CardValue[] = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', 'Draw+2'];
+        const Black = 'Black';
+        const values: CardValue[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', 'Draw+2'];
+        const zero = '0';
+        const blackChange = 'black-change';
+        const blackPlusFourChange = 'black+4-change';
+        for (let i = 0; i < 4; i++) {
+            this.cards.push( {
+                color: 'Black',
+                value: 'black+4-change',
+            });
+        }
+        for (let i = 0; i < 4; i++) {
+            this.cards.push({
+                color: 'Black',
+                value: 'black-change',
+            });
+        }
+        for (const color of colors) {
+            this.cards.push({
+                color: color,
+                value: zero,
+            });
+            for (let i = 0; i < 2; i++) {
+                for (const value of values) {
+                    this.cards.push({
+                        color: color,
+                        value: value,
+                    });
+
+                }
+            }
+        }
+    }
+    shuffle (){
 
     }
 }
