dc51da6dee6c4bb8812f60d02e4f605458177b96
Xonex <prokoptsov.k.v.-imo51@edu.kpi.ua>
2026-03-24 12:42:26 +0200

    Change color and shuffling cards

--- backend/src/game/Deck.ts
+++ backend/src/game/Deck.ts
@@ -1,5 +1,5 @@
 export class Deck {}
-type CardColor = 'Red' | 'Green' | 'Blue' | 'Yellow' | 'Black';
+type CardColor = 'crimson' | 'yellow' | 'orange' | 'purple' | 'Black';
 type CardValue = |'0'| '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | 'Skip' | 'Reverse' | 'Draw+2' | 'black-change' | 'black+4-change';
 interface Card{
     color: CardColor;
@@ -9,7 +9,7 @@ class Decka {
     private cards: Card[] = [];
 
     constructor() {
-        const colors: CardColor[] = ['Red', 'Green', 'Blue', 'Yellow'];
+        const colors: CardColor[] = ['crimson', 'yellow', 'orange', 'purple'];
         const Black = 'Black';
         const values: CardValue[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'Skip', 'Reverse', 'Draw+2'];
         const zero = '0';
@@ -43,7 +43,22 @@ class Decka {
             }
         }
     }
+    public getCards() {
+        return this.cards;
+    }
     shuffle (){
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+
+            const temp = this.cards[i] as Card;
+            this.cards[i] = this.cards[j] as Card;
+            this.cards[j] = temp;
+        }
 
     }
+
 }
+
+const myDeck = new Decka();
+myDeck.shuffle(); // Вызываем перемешивание
+console.table(myDeck.getCards());
