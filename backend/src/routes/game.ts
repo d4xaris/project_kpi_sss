d56d8e38d8e89819fd9f139108e6d266764dcd54
Honike-1 <romakizilenkov@gmail.com>
2026-04-18 22:02:57 +0300

    feat: made hook for token checking in game.ts

--- backend/src/routes/game.ts
+++ backend/src/routes/game.ts
@@ -1,34 +1,32 @@
 import type { FastifyInstance } from "fastify";
 
 export default async function gameRoutes(app: FastifyInstance) {
-  app.post(
-    "/game/create",
-    { preValidation: [(app as any).authenticate] },
-    async (request, reply) => {
-      const user = request.user as {
-        id: number;
-        nickname: string;
-      };
+  app.addHook("preValidation", (app as any).authenticate);
 
-      const { sessionName, maxPlayers } = request.body as {
-        sessionName: string;
-        maxPlayers: number;
-      };
+  app.post("/game/create", async (request, reply) => {
+    const user = request.user as {
+      id: number;
+      nickname: string;
+    };
 
-      const game = await app.prisma.GameSession.create({
-        data: {
-          sessionName: sessionName,
-          maxPlayers: maxPlayers,
-          hostId: user.id,
-          players: {
-            connect: { id: user.id },
-          },
+    const { sessionName, maxPlayers } = request.body as {
+      sessionName: string;
+      maxPlayers: number;
+    };
+
+    const game = await app.prisma.GameSession.create({
+      data: {
+        sessionName: sessionName,
+        maxPlayers: maxPlayers,
+        hostId: user.id,
+        players: {
+          connect: { id: user.id },
         },
-      });
+      },
+    });
 
-      reply.status(201).send(game);
-    },
-  ); // this thing will be connected to front but rn idk how, just mark for future
+    reply.status(201).send(game);
+  }); // this thing will be connected to front but rn idk how, just mark for future
 
   app.get("/game/:id/status", async (request, reply) => {
     const { id } = request.params as { id: string };
