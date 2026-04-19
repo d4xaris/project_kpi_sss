e8469b879495265d61e5911d23afbd6bfe0d6559
d4xaris <katerynaiakhnovska@gmail.com>
2026-04-19 20:12:30 +0300

    fixed error with backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,18 @@
 import Fastify from "fastify";
+import 'dotenv/config';
 
 import gameRoutes from "./routes/game.js";
 import authRoutes from "./routes/auth.js";
-
+import authPlugin from "./plugins/jwt.js";
 const app = Fastify({ logger: true });
+
+await app.register(authPlugin);
 
 app.register(gameRoutes, { prefix: "/game" });
 app.register(authRoutes, { prefix: "/auth" });
 
-// plugins
+
+
 // routes
 // socket
 
