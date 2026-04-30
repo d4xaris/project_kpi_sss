9f9f8c987805dd624461eb910208aeb506367396
Honike-1 <romakizilenkov@gmail.com>
2026-04-30 22:18:57 +0300

    feat: added route /auth/logout and some minor changes

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,6 +1,7 @@
 import type { FastifyInstance } from "fastify";
+import { request } from "node:http";
 
-const registrationShema = {
+const registrationSchema = {
   body: {
     type: "object",
     required: ["login", "nickname", "password"],
@@ -12,11 +13,22 @@ const registrationShema = {
   },
 };
 
+const loginSchema = {
+  body: {
+    type: "object",
+    required: ["login", "password"],
+    properties: {
+      login: { type: "string", minLength: 3, maxLength: 18 },
+      password: { type: "string", minLength: 3, maxLength: 18 },
+    },
+  },
+};
+
 export default async function authRoutes(app: FastifyInstance) {
   // route for registration
   app.post(
     "/auth/registration",
-    { schema: registrationShema },
+    { schema: registrationSchema },
     async (request, reply) => {
       //can change the path of this route if need
       const { login, nickname, password } = request.body as any;
@@ -57,40 +69,58 @@ export default async function authRoutes(app: FastifyInstance) {
   );
 
   // route for login
-  app.post("/auth/login", async (request, reply) => {
+  app.post("/auth/login", { schema: loginSchema }, async (request, reply) => {
     const { login, password } = request.body as {
       login: string;
       password: string;
     };
 
-    const user = await app.prisma.User.findUnique({
-      where: { login: login },
-    });
+    try {
+      const user = await app.prisma.User.findUnique({
+        where: { login: login },
+      });
 
-    if (!user) {
-      //if there is no such user
-      return reply.status(401).send({ error: "Incorrect login or password" });
-    }
+      if (!user) {
+        //if there is no such user
+        return reply.status(401).send({ error: "Incorrect login or password" });
+      }
 
-    if (user.password !== password) {
-      //if password is incorrect
-      return reply.status(401).send({ error: "Incorrect login or password" });
-    }
+      if (user.password !== password) {
+        //if password is incorrect
+        return reply.status(401).send({ error: "Incorrect login or password" });
+      }
 
-    const token = app.jwt.sign({
-      id: user.id,
-      nickname: user.nickname,
-    });
-
-    return reply.status(200).send({
-      message: "success",
-      token: token,
-      user: {
+      const token = app.jwt.sign({
         id: user.id,
         nickname: user.nickname,
-      },
-    });
+      });
+
+      return reply.status(200).send({
+        message: "success",
+        token: token,
+        user: {
+          id: user.id,
+          nickname: user.nickname,
+        },
+      });
+    } catch (error) {
+      // for server errors
+      return reply.status(500).send({ error: "Server error" });
+    }
   });
+
+  app.post(
+    "/auth/logout",
+    { preValidation: [(app as any).authenticate] },
+    async (request, reply) => {
+      try {
+        return reply.status(200).send({ message: "Logged out succesfully" });
+      } catch (error) {
+        // for server errors
+        return reply.status(500).send({ error: "Server error" });
+      }
+    },
+  );
 
   app.get(
     "/auth/me",
